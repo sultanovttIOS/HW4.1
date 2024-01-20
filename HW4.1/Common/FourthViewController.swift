533d6b04e8ebed91f7b7178d3e55736c8eee05f5
sultanovttIOS <alisher192377@gmail.com>
2024-01-21 00:02:08 +0600

    Corrected constraints 4 screen

--- HW4.1/Common/FourthViewController.swift
+++ HW4.1/Common/FourthViewController.swift
@@ -31,6 +31,12 @@ class FourthViewController: UIViewController {
     
     private lazy var borderView2 = MakerView.shared.makerBorder()
     
+    private lazy var newPassVector = MakerView.shared.makerVector(image: UIImage(named: "vector"))
+    
+    private lazy var confPassVector = MakerView.shared.makerVector(image: UIImage(named: "vector"))
+
+    private lazy var submitButton = MakerView.shared.makerButton(setTitle: "Submit", tintColor: UIColor.white, backgroundColor: UIColor(hex: "#4AB07E"), cornerRadius: 15)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -47,14 +53,50 @@ class FourthViewController: UIViewController {
         setupTfView()
         setupNewPassLabel()
         setupNewPassTf()
-        //setupConfPassLabel()
-//        setupConfPassTf()
-//        setupBrdView2()
+        setupBrdView()
+        setupConfPassLabel()
+        setupConfPassTf()
+        setupBrdView2()
+        setupNewPassVector()
+        setupConfPassVector()
+        setupSubmitButton()
+
     }
+    private func setupSubmitButton() {
+        tfView.addSubview(submitButton)
+        submitButton.snp.makeConstraints { make in
+            make.width.equalTo(95)
+            make.height.equalTo(35)
+            make.centerX.equalTo(tfView.snp.centerX)
+            make.top.equalTo(borderView2.snp.bottom).offset(67)
+            make.bottom.equalTo(tfView.snp.bottom).offset(-107)
+
+        }
+    }
+    private func setupConfPassVector() {
+        tfView.addSubview(confPassVector)
+        confPassVector.snp.makeConstraints { make in
+            make.centerY.equalTo(confPassTf.snp.centerY)
+            make.trailing.equalTo(tfView.snp.trailing).offset(-30)
+            make.width.equalTo(18)
+            make.height.equalTo(13)
+        }
+    }
+    private func setupNewPassVector() {
+        tfView.addSubview(newPassVector)
+        newPassVector.snp.makeConstraints { make in
+            make.centerY.equalTo(newPassTf.snp.centerY)
+            make.trailing.equalTo(tfView.snp.trailing).offset(-30)
+            make.width.equalTo(18)
+            make.height.equalTo(13)
+        }
+    }
+
+    
     private func setupBrdView2() {
         tfView.addSubview(borderView2)
         borderView2.snp.makeConstraints { make in
-            make.top.equalTo(confPassTf.snp.bottom).offset(13)
+            make.top.equalTo(confPassTf.snp.bottom).offset(10)
             make.horizontalEdges.equalToSuperview().inset(23)
             make.height.equalTo(1)
         }
@@ -62,7 +104,7 @@ class FourthViewController: UIViewController {
     private func setupConfPassTf() {
         tfView.addSubview(confPassTf)
         confPassTf.snp.makeConstraints { make in
-            make.top.equalTo(confPassLabel.snp.bottom).offset(13)
+            make.top.equalTo(confPassLabel.snp.bottom).offset(10)
             make.leading.equalTo(tfView.snp.leading).offset(23)
             make.trailing.equalTo(tfView.snp.trailing).offset(-224)
             make.height.equalTo(20)
@@ -74,14 +116,15 @@ class FourthViewController: UIViewController {
         confPassLabel.snp.makeConstraints { make in
             make.top.equalTo(borderView.snp.bottom).offset(25)
             make.leading.equalTo(tfView.snp.leading).offset(23)
-            make.trailing.equalTo(tfView.snp.trailing).offset(213)
+            make.trailing.equalTo(tfView.snp.trailing).offset(-213)
             make.height.equalTo(15)
         }
     }
+    
     private func setupBrdView() {
         tfView.addSubview(borderView)
         borderView.snp.makeConstraints { make in
-            make.top.equalTo(newPassTf.snp.bottom).offset(13)
+            make.top.equalTo(newPassTf.snp.bottom).offset(10)
             make.horizontalEdges.equalToSuperview().inset(23)
             make.height.equalTo(1)
         }
@@ -89,7 +132,7 @@ class FourthViewController: UIViewController {
     private func setupNewPassTf() {
         tfView.addSubview(newPassTf)
         newPassTf.snp.makeConstraints { make in
-            make.top.equalTo(newPasswordLabel.snp.bottom).offset(13)
+            make.top.equalTo(newPasswordLabel.snp.bottom).offset(10)
             make.leading.equalTo(tfView.snp.leading).offset(23)
             make.trailing.equalTo(tfView.snp.trailing).offset(-224)
             make.height.equalTo(20)
@@ -108,10 +151,10 @@ class FourthViewController: UIViewController {
     private func setupTfView() {
         view.addSubview(tfView)
         tfView.snp.makeConstraints { make in
+            make.height.equalTo(466)
             make.bottom.equalTo(view.snp.bottom)
             make.horizontalEdges.equalToSuperview()
-            make.height.equalTo(466)
-            
+//            make.top.equalTo(smsTfView.snp.bottom).offset(43)
         }
     }
     private func setupSmsTf() {
@@ -126,7 +169,7 @@ class FourthViewController: UIViewController {
     private func setupSmsTfView() {
         view.addSubview(smsTfView)
         smsTfView.snp.makeConstraints { make in
-            make.top.equalTo(image.snp.bottom).offset(34)
+            make.bottom.equalTo(view.snp.bottom).offset(-509)
             make.horizontalEdges.equalToSuperview().inset(42)
             make.height.equalTo(45)
         }
@@ -134,9 +177,10 @@ class FourthViewController: UIViewController {
     private func setupImage() {
         view.addSubview(image)
         image.snp.makeConstraints { make in
-            make.top.equalTo(view.snp.top).offset(84)
-            make.horizontalEdges.equalToSuperview().inset(106)
-            make.height.width.equalTo(180)
+            make.top.equalTo(view.snp.top).offset(64)
+            make.height.width.equalTo(148)
+            make.centerX.equalTo(view.snp.centerX)
+//            make.horizontalEdges.equalToSuperview().inset(106)
         }
     }
 }
